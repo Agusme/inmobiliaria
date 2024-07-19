035d6a2379ee05b1bbf9a53d5c519e10d63ae30b
agusmena <agusmena60@mail.com>
2024-07-19 12:50:34 -0300

    add changes

--- src/components/views/terrenos/TerrenosLules.jsx
+++ src/components/views/terrenos/TerrenosLules.jsx
@@ -1,32 +1,32 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "../../../config/axiosInit"
-import {  Col, Container,  Row } from "react-bootstrap";
+import axios from "../../../config/axiosInit";
+import { Col, Container, Row } from "react-bootstrap";
 import { TbBathFilled } from "react-icons/tb";
 import { IoBed, IoCallOutline } from "react-icons/io5";
 import { FaWhatsapp } from "react-icons/fa";
 import { Fade } from "react-awesome-reveal";
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 
 const TerrenosLules = () => {
-    const URL = import.meta.env.VITE_API_BMZ;
-    const { id } = useParams();
-    const [property, setProperty] = useState(null);
-    useEffect(() => {
-        getOne();
-      }, []);
-      const getOne = async () => {
-        try {
-          const res = await axios.get(`${URL}/${id}`);
-          setProperty(res.data);
-        } catch (error) {
-          console.log(error);
-        }
-      };
-    return (
-        <div>
-    <div className="py-5">
+  const URL = import.meta.env.VITE_API_BMZ;
+  const { id } = useParams();
+  const [property, setProperty] = useState(null);
+  useEffect(() => {
+    getOne();
+  }, []);
+  const getOne = async () => {
+    try {
+      const res = await axios.get(`${URL}/${id}`);
+      setProperty(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <div className="py-5">
         <Container className="mt-5 d-flex justify-content-center align-items-center">
           {property ? (
             <Container className="bg-white pb-5">
@@ -77,39 +77,37 @@ const TerrenosLules = () => {
                       )}
                     </p>
                     <p className="letra-azul">{property.description}</p>
-                    <p className="letra-azul">EMPRESA CONSTRUCTORA. MACROTECH SRL.</p>
+                    <p className="letra-azul">
+                      EMPRESA CONSTRUCTORA. MACROTECH SRL.
+                    </p>
 
                     <Col lg={6} md={6} sm={12}>
                       <Fade>
-                      <ul className="list-unstyled d-flex letra-azul fs-5">
-         <p className="pt-4 mx-3">Más info.</p>
-                  <a
-                    href="https://api.whatsapp.com/send/?phone=543815633407&text=Hola%21+Quisiera+saber+m%C3%A1s+informaci%C3%B3n+de+&type=phone_number&app_absent=0"
-                    target="_blank"
-                    className="letra-azul"
-                  >
-                    {" "}
-                    <li className="link-hover pt-4">
-                      {" "}
-                      <FaWhatsapp className="fs-2 me-3" />
-                    </li>
-                  </a>
-                  
-                  <a
-                      href="tel:+543815633407"
-                      className="letra-azul"
-                    >
-                  <li className="link-hover pt-4">
-                   
-                      <IoCallOutline className="fs-2 me-3"  />
-                  </li>
-                  </a>
-              
-  
-                </ul>
+                        <ul className="list-unstyled d-flex letra-azul fs-5">
+                            <li className="list-style-none">
+                                JULIA - 
+                            </li>
+                          <p className="pt-4 mx-3">Más info.</p>
+                          <a
+                            href="https://api.whatsapp.com/send/?phone=543815633407&text=Hola%21+Quisiera+saber+m%C3%A1s+informaci%C3%B3n+de+&type=phone_number&app_absent=0"
+                            target="_blank"
+                            className="letra-azul"
+                          >
+                            {" "}
+                            <li className="link-hover pt-4">
+                              {" "}
+                              <FaWhatsapp className="fs-2 me-3" />
+                            </li>
+                          </a>
+
+                          <a href="tel:+543815633407" className="letra-azul">
+                            <li className="link-hover pt-4">
+                              <IoCallOutline className="fs-2 me-3" />
+                            </li>
+                          </a>
+                        </ul>
                       </Fade>
-    
-                </Col>
+                    </Col>
                   </Col>
                   <Col lg={6} md={6} sm={12}>
                     <div className="px-3">
@@ -131,8 +129,9 @@ const TerrenosLules = () => {
             <p className="text-white"> Loading...</p>
           )}
         </Container>
-      </div>        </div>
-    );
+      </div>{" "}
+    </div>
+  );
 };
 
 export default TerrenosLules;
